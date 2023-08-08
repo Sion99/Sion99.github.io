9ea0c0209bfa2b888e1084bf34e5223bb7a351ad
sion99 <kmicety1@gmail.com>
2023-08-09 00:02:49 +0900

    new

---
+++ receipt.js
@@ -0,0 +1,41 @@
+const modal = document.getElementById('modal');
+const closeModalBtn = document.getElementById('close-modal');
+const realSubmitBtn = document.getElementById('realSubmit');
+const fileInput = document.getElementById('chooseFile');
+
+
+closeModalBtn.addEventListener('click', () => {
+	modal.style.display = "none";
+	document.body.style.overflow = "auto";
+})
+
+realSubmitBtn.onclick = uploadFile;
+
+function uploadFile() {
+	const formData = new FormData;
+	var file = fileInput.files[0];
+	formData.append('file', file);
+	fetch('http://127.0.0.1:8000/post', {
+		method: 'POST',
+		cache: 'no-cache',
+		body: formData // body 부분에 폼데이터 변수를 할당
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+		});
+}
+
+
+function loadFile(input) {
+	modal.style.display = "block";
+	document.body.style.overflow = "hidden";
+	var file = input.files[0];
+
+	var newImage = document.createElement("img");
+	newImage.setAttribute("class", 'img');
+
+	newImage.src = URL.createObjectURL(file);
+	var container = document.getElementById('image-show');
+	container.appendChild(newImage);
+};
